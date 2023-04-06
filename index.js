b49a2033cd27b1f1acd5b547a3608314359d904a
MD HAQIQUR RAHMAN <115725604+mdhaqiqur@users.noreply.github.com>
2023-04-06 15:12:00 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,61 @@
+let form = document.getElementById("form");
+let textInput = document.getElementById("textInput");
+let dateInput = document.getElementById("dateInput");
+let textarea = document.getElementById("textarea");
+let msg = document.getElementById("msg");
+let tasks = document.getElementById("tasks");
+let add = document.getElementById("add");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    formValidation();
+  });
+  
+  let formValidation = () => {
+    if (textInput.value === "") {
+      console.log("failure");
+      msg.innerHTML = "Task cannot be blank";
+    } else {
+      console.log("success");
+      msg.innerHTML = "";
+    }
+  };
+  let data = [];
+
+let acceptData = () => {
+  data.push({
+    text: textInput.value,
+    date: dateInput.value,
+    description: textarea.value,
+  });
+
+  localStorage.setItem("data", JSON.stringify(data));
+
+  console.log(data);
+};
+let formValidation = () => {
+
+   
+     else {
+  
+      
+  
+      acceptData();
+    }
+  };
+  let formValidation = () => {
+
+   
+     else {
+  
+      
+  
+      acceptData();
+      add.setAttribute("data-bs-dismiss", "modal");
+      add.click();
+  
+      (() => {
+        add.setAttribute("data-bs-dismiss", "");
+      })();
+    }
+  };
